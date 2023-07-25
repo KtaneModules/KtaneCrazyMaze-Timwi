3edc1a47350f7545e4a95c87a59a19a80ee07c10
Timwi <github@timwi.de>
2023-07-25 12:52:35 +0200

    First playable version

--- Assets/PolygonalMazeScript.cs
+++ Assets/PolygonalMazeScript.cs
@@ -1,120 +1,186 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using KModkit;
+
 using Rnd = UnityEngine.Random;
 
 public class PolygonalMazeScript : MonoBehaviour
 {
-    static int _moduleIdCounter = 1;
-    int _moduleID = 0;
-
     public KMBombModule Module;
     public KMBombInfo Bomb;
     public KMAudio Audio;
     public KMSelectable[] Arrows;
-    public TextMesh[] Numbers;
+    public KMSelectable Bridge;
+    public TextMesh CurCellText;
+    public TextMesh GoalCellText;
+    public SpriteRenderer CurrentCell;
+    public Sprite[] Sprites;
+    public KMRuleSeedable RuleSeedable;
 
-    private int CurrentShape = 3;
+    private static int _moduleIdCounter = 1;
+    private int _moduleID = 0;
+    private bool _moduleSolved = false;
 
-    private enum ShapeIxs
-    {
-        Square,
-        Triangle,
-        Hexagon,
-        Octagon,
-        Diamond,
-        Rhombus,
-        Kite,
-        Cairo
-    }
-    private static readonly string[] Geometries = new string[] { "Square", "Triangle", "Hexagon", "Octagon", "Rhombus", "Kite", "Cairo" };
-    private struct Arrow
-    {
-        /// <summary>
-        /// Position of the arrow.
-        /// </summary>
-        public Vector3 Position;
-        /// <summary>
-        /// Rotation of the arrow.
-        /// </summary>
-        public Vector3 Rotation;
-        public Arrow(Vector3 position, Vector3 rotation)
-        {
-            Position = position;
-            Rotation = rotation;
-        }
-    }
-    private static readonly Dictionary<int, Arrow[]> ArrowPositions = new Dictionary<int, Arrow[]>
-    {
-        { (int)ShapeIxs.Square, new Arrow[]{ new Arrow(new Vector3(0, 0.0154f, 0.05f), new Vector3(90, 0, 0)), new Arrow(new Vector3(0.05f, 0.0154f, 0), new Vector3(90, 90, 0)), new Arrow(new Vector3(0, 0.0154f, -0.05f), new Vector3(90, 180, 0)), new Arrow(new Vector3(-0.05f, 0.0154f, 0), new Vector3(90, 270, 0)) } },
-        { (int)ShapeIxs.Octagon, new Arrow[]{ new Arrow(new Vector3(0, 0.0154f, 0.05f), new Vector3(90, 0, 0)), new Arrow(new Vector3(1 / Mathf.Sqrt(2) * 0.05f, 0.0154f, 1 / Mathf.Sqrt(2) * 0.05f), new Vector3(90, 45, 0)), new Arrow(new Vector3(0.05f, 0.0154f, 0), new Vector3(90, 90, 0)), new Arrow(new Vector3(1 / Mathf.Sqrt(2) * 0.05f, 0.0154f, -1 / Mathf.Sqrt(2) * 0.05f), new Vector3(90, 135, 0)), new Arrow(new Vector3(0, 0.0154f, -0.05f), new Vector3(90, 180, 0)), new Arrow(new Vector3(-1 / Mathf.Sqrt(2) * 0.05f, 0.0154f, -1 / Mathf.Sqrt(2) * 0.05f), new Vector3(90, 225, 0)), new Arrow(new Vector3(-0.05f, 0.0154f, 0), new Vector3(90, 270, 0)), new Arrow(new Vector3(-1 / Mathf.Sqrt(2) * 0.05f, 0.0154f, 1 / Mathf.Sqrt(2) * 0.05f), new Vector3(90, 315, 0)) } }
-    };
+    private int _currentCell;
+    private int _goalCell;
+    private bool _showingGoal;
+
+    private HashSet<int>[] _passable;
+    private string[] _cellLetters;
 
     void Awake()
     {
         _moduleID = _moduleIdCounter++;
-        var contents = Module.transform.Find("Contents").transform;
-        PlaceArrows();
         for (int i = 0; i < Arrows.Length; i++)
+            Arrows[i].OnInteract += ArrowPress(i, Arrows[i], "move");
+        Bridge.OnInteract += ArrowPress(-1, Bridge, "bridge");
+        _passable = Enumerable.Range(0, 26 * 26).Select(ix => new HashSet<int>()).ToArray();
+    }
+
+    private KMSelectable.OnInteractHandler ArrowPress(int pos, KMSelectable sel, string sound)
+    {
+        var isBridge = pos == -1;
+        var upper = isBridge ? "Traversing bridge" : "Going";
+        var lower = isBridge ? "traverse bridge" : "go";
+
+        return delegate
         {
-            int x = i;
-            Arrows[x].OnInteract += delegate { ArrowPress(x); return false; };
-        }
-        for (int i = 0; i < Numbers.Length; i++)
-            StartCoroutine(NumbersAnim(i, i / 2f));
-        contents.localScale = Vector3.zero;
-        Module.OnActivate += delegate { contents.localScale = Vector3.one; };
-    }
+            Audio.PlaySoundAtTransform(sound, sel.transform);
+            sel.AddInteractionPunch(.3f);
+            if (_moduleSolved)
+                return false;
 
-    // Use this for initialization
-    void Start()
-    {
+            var goingTo = isBridge
+                ? CellTransitions.All[_currentCell].BridgeDestination
+                : CellTransitions.All[_currentCell].Neighbors[pos].ToCell;
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    void ArrowPress(int pos)
-    {
-        Audio.PlaySoundAtTransform("move", Arrows[pos].transform);
-    }
-
-    void PlaceArrows()
-    {
-        for (int i = 0; i < Arrows.Length; i++)
-        {
-            if (ArrowPositions[CurrentShape].Length > i)
+            if (goingTo != null && _passable[_currentCell].Contains(goingTo.Value))
             {
-                Arrows[i].transform.localScale = new Vector3(0.5f, 0.5f, 0.005f);
-                Arrows[i].transform.localPosition = ArrowPositions[CurrentShape][i].Position;
-                Arrows[i].transform.localEulerAngles = ArrowPositions[CurrentShape][i].Rotation;
+                Debug.LogFormat(@"[Polygonal Maze #{0}] {3} from {1} to {2}.", _moduleID, _cellLetters[_currentCell], _cellLetters[goingTo.Value], upper);
+                if (isBridge)
+                    _showingGoal = !_showingGoal;
+                SetCell(goingTo.Value);
+                if (goingTo.Value == _goalCell)
+                {
+                    Debug.LogFormat(@"[Polygonal Maze #{0}] Module solved.", _moduleID);
+                    Module.HandlePass();
+                    _moduleSolved = true;
+                }
+            }
+            else if (goingTo != null)
+            {
+                Debug.LogFormat(@"[Polygonal Maze #{0}] Attempt to {3} from {1} to {2}. Strike.", _moduleID, _cellLetters[_currentCell], _cellLetters[goingTo.Value], lower);
+                Module.HandleStrike();
             }
             else
-                Arrows[i].transform.localScale = new Vector3();
-        }
+            {
+                Debug.LogFormat(@"[Polygonal Maze #{0}] Attempt to cross a non-existent bridge from {1}. Strike.", _moduleID, _cellLetters[_currentCell]);
+                Module.HandleStrike();
+            }
+
+            return false;
+        };
     }
 
-    private IEnumerator NumbersAnim(int ix, float offset, float duration = 2f, float intensity = 0.0003f, float adjustX = 0.02f, float adjustZ = 0.02f, float scaleX = 2f, float scaleZ = 1f)
+    void Start()
     {
-        offset *= duration;
+        // ## RULE SEED
+        var rnd = RuleSeedable.GetRNG();
+        var cells = Enumerable.Range(0, 676).ToList();
+        var links = Enumerable.Range(0, 676)
+            .Select(cellIx => CellTransitions.All[cellIx])
+            .Select(tr => tr.BridgeDestination == null ? tr.Neighbors.Select(n => n.ToCell) : tr.Neighbors.Select(n => n.ToCell).Concat(new[] { tr.BridgeDestination.Value }))
+            .Select(cs => cs.OrderBy(c => c).ToArray())
+            .ToArray();
+
+        // Find a random starting cell
+        var startCellIx = rnd.Next(0, cells.Count);
+        var startCell = cells[startCellIx];
+
+        // Maze algorithm starts here
+        var todo = new List<int> { startCell };
+        cells.RemoveAt(startCellIx);
+
+        while (cells.Count > 0)
+        {
+            var ix = rnd.Next(0, todo.Count);
+            var cell = todo[ix];
+
+            var availableLinks = links[cell].Where(otherCell => cells.Contains(otherCell)).ToArray();
+            if (availableLinks.Length == 0)
+                todo.RemoveAt(ix);
+            else
+            {
+                var otherCell = availableLinks[availableLinks.Length == 1 ? 0 : rnd.Next(0, availableLinks.Length)];
+                _passable[cell].Add(otherCell);
+                _passable[otherCell].Add(cell);
+                cells.Remove(otherCell);
+                todo.Add(otherCell);
+            }
+        }
+
+        var letters = Enumerable.Range(0, 26).Select(c => (char) ('A' + c));
+        _cellLetters = rnd.ShuffleFisherYates(letters.SelectMany(ltr => letters.Select(ltr2 => ltr + "" + ltr2)).ToArray());
+        // End rule seed
+
+        // Decide on a start cell
+        SetCell(Rnd.Range(0, 26 * 26));
+
+        // Decide on a goal cell that is a certain distance away
+        const int minAllowedDistance = 12;
+        const int maxAllowedDistance = 17;
+        var visited = new HashSet<int> { _currentCell };
+        var chooseFrom = new HashSet<int>();
+        var curDist = 0;
+        while (curDist <= maxAllowedDistance)
+        {
+            var newCells = visited.SelectMany(cel => _passable[cel]).Except(visited).ToArray();
+            if (curDist >= minAllowedDistance)
+                chooseFrom.UnionWith(newCells);
+            visited.UnionWith(newCells);
+            curDist++;
+        }
+        _goalCell = chooseFrom.PickRandom();
+
+        Debug.LogFormat(@"[Polygonal Maze #{0}] Start cell: {1}", _moduleID, _cellLetters[_currentCell]);
+        Debug.LogFormat(@"[Polygonal Maze #{0}] Goal cell: {1}", _moduleID, _cellLetters[_goalCell]);
+
+        StartCoroutine(CellTextAnimation(CurCellText.transform, 0, adjustX: -.02f, adjustZ: .02f));
+        StartCoroutine(CellTextAnimation(GoalCellText.transform, 1, adjustX: .02f, adjustZ: -.02f));
+    }
+
+    void SetCell(int cell)
+    {
+        var cellTransitions = CellTransitions.All[cell];
+        var transitions = cellTransitions.Neighbors;
+
+        for (int arIx = 0; arIx < Arrows.Length; arIx++)
+        {
+            if (arIx >= transitions.Length)
+                Arrows[arIx].gameObject.SetActive(false);
+            else
+            {
+                Arrows[arIx].transform.localPosition = new Vector3(transitions[arIx].ArrowX, .0154f, -transitions[arIx].ArrowY);
+                Arrows[arIx].transform.localEulerAngles = new Vector3(0, transitions[arIx].ArrowAngle, 0);
+                Arrows[arIx].gameObject.SetActive(true);
+            }
+        }
+        _currentCell = cell;
+        CurrentCell.sprite = Sprites[cell];
+    }
+
+    private IEnumerator CellTextAnimation(Transform trf, float offset, float duration = 2f, float intensity = 0.018f, float adjustX = 0.02f, float adjustZ = 0.02f)
+    {
         while (true)
         {
-            float timer = 0;
-            while (timer < duration)
-            {
-                yield return null;
-                timer += Time.deltaTime;
-                float x = Mathf.Rad2Deg * Mathf.Cos(((timer + offset) % duration) / duration * 2 * Mathf.PI) * intensity;
-                float z = Mathf.Rad2Deg * Mathf.Sin(((timer + offset) % duration) / duration * 2 * Mathf.PI) * intensity;
-                Numbers[ix].transform.localPosition = new Vector3((x * scaleX) + new float[] { -adjustX, adjustX }[ix], Numbers[ix].transform.localPosition.y, (z * scaleZ) + new float[] { adjustZ, -adjustZ }[ix]);
-            }
+            yield return null;
+            var timer = Time.time;
+            float x = Mathf.Cos((timer + offset) / duration * 2 * Mathf.PI) * intensity;
+            float z = Mathf.Sin((timer + offset) / duration * 2 * Mathf.PI) * intensity;
+            trf.localPosition = new Vector3(x + adjustX, trf.localPosition.y, z + adjustZ);
+            CurCellText.text = _moduleSolved ? "G" : _showingGoal ? "??" : _cellLetters[_currentCell];
+            GoalCellText.text = _moduleSolved ? "G" : _showingGoal ? _cellLetters[_goalCell] : "??";
         }
     }
 }
---
+++ Assets/Transition.cs
@@ -0,0 +1,15 @@
+struct Transition
+{
+    public int ToCell { get; private set; }
+    public float ArrowX { get; private set; }
+    public float ArrowY { get; private set; }
+    public float ArrowAngle { get; private set; }
+
+    public Transition(int toCell, float arrowX, float arrowY, float arrowAngle)
+    {
+        ToCell = toCell;
+        ArrowX = arrowX;
+        ArrowY = arrowY;
+        ArrowAngle = arrowAngle;
+    }
+}
